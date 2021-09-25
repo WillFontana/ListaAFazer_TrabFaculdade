8adf285e8be6ee09ea1023c37b6a66433c727f30
WillFontana <willyanfontana@gmail.com>
2021-09-25 17:46:49 -0300

    Corrige estilos do campo de add

--- components/TodoList/ListItem.js
+++ components/TodoList/ListItem.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import Button from "../Button";
 import { AntDesign } from "@expo/vector-icons";
 import { styles } from "./styles";
+import capitalize from "../../utils/capitalize";
 
 const ListItem = ({ item, handleChangeStatus }) => {
   return (
@@ -14,20 +15,24 @@ const ListItem = ({ item, handleChangeStatus }) => {
         <Text style={[styles.text, { marginBottom: 2 }]}>
           Data de criação: {item.data}
         </Text>
-        <Text style={styles.text}>Status da tarefa: {item.status}</Text>
+        <Text style={styles.text}>
+          Status da tarefa: {item.status.capitalize()}
+        </Text>
       </View>
-      <View>
-        <Button
-          complementStyle={{ marginBottom: 5, backgroundColor: "#BDE9BD66" }}
-          icon={<AntDesign name="check" size={16} color="black" />}
-          onPress={() => handleChangeStatus(item.id, "concluido")}
-        />
-        <Button
-          complementStyle={{ backgroundColor: "#E9BDBD66" }}
-          icon={<AntDesign name="close" size={16} color="black" />}
-          onPress={() => handleChangeStatus(item.id, "cancelado")}
-        />
-      </View>
+      {item.status === "aberto" && (
+        <View>
+          <Button
+            complementStyle={{ marginBottom: 5, backgroundColor: "#BDE9BD66" }}
+            icon={<AntDesign name="check" size={16} color="black" />}
+            onPress={() => handleChangeStatus(item.id, "concluido")}
+          />
+          <Button
+            complementStyle={{ backgroundColor: "#E9BDBD66" }}
+            icon={<AntDesign name="close" size={16} color="black" />}
+            onPress={() => handleChangeStatus(item.id, "cancelado")}
+          />
+        </View>
+      )}
     </View>
   );
 };
